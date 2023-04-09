cfb01c70cf16b57d6495df98274f58707ed42318
chellick <matsupus@gmail.com>
2023-04-09 18:13:50 +0300

    upd.

--- codes/base.py
+++ codes/base.py
@@ -1,56 +1,54 @@
+class Vector(list):
+    def __init__(self, array):
+        super().__init__(array)
 
+    def __add__(self, other):
+        return Vector(a + b for a, b in zip(self, other))
+    
+    def __sub__(self, other):
+        return Vector(a - b for a, b in zip(self, other))
 
+    def __mul__(self, other):
+        return Vector(a * b for a, b in zip(self, other))
+    
+    def __truediv__(self, other):
+        return Vector(a / b for a, b in zip(self, other))
+    
+    def __eq__(self, other):
+        return all(a == b for a, b in zip(self, other))
+    
+    def __ne__(self, other):
+        return not self == other
 
-class Vector:
+    # def __repr__(self):
+    #     return '[' + ', '.join(map(str, self)) + ']'
+
+# Пример использования
+v1 = Vector([1, 2, 3])
+v2 = Vector([4, 5, 6])
+
+# print(v1)  # Вывод: [1, 2, 3]
+# print(v1 + v2)  # Вывод: [5, 7, 9]
+
+    
+    
+
+class Matrix:
     def __init__(self, array):
         self.array = array
 
     def __getattributes__(self):
         return self.__dict__
-    
-    def __add__(self, other):
-        result = Vector([])
-        result.array = list(a + b for a, b in zip(self.array, other.array))
-        return result.array
-    
-    def __sub__(self, other):
-        result = Vector([])
-        result.array = list(a - b for a, b in zip(self.array, other.array))
-        return result.array
-
-    def __mul__(self, other):
-        result = Vector([])
-        result.array = list(a * b for a, b in zip(self.array, other.array))
-        return result.array
-    
-    def __truediv__(self, other):
-        result = Vector([])
-        result.array = list(a / b for a, b in zip(self.array, other.array))
-        return result.array
-    
-    def __eq__(self, other):
-        for a, b in zip(self.array, other.array):
-            if a == b:
-                result = True
-            else: 
-                result = False
-                return result
-        return result
-    
-    def __ne__(self, other):
-        for a, b in zip(self.array, other.array):
-            if a == b:
-                result = False
-            else: 
-                result = True
-                return result
-        return result
 
 
-a = Vector([5, 6, 7])
+a = Vector([8, 6, 7])
 b = Vector([5, 6, 7])
-print(a == b)
-print(a != b)
+c = Matrix([a, b])
+
+
+print(len(a))
+print(c.array)
+
 
 
 
