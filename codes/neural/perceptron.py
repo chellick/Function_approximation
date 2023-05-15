7f54e4b6cbe3ce98367fc4a8a005ffaf82d8650f
chellick <matsupus@gmail.com>
2023-05-15 21:30:55 +0300

    forward upd.

--- codes/neural/perceptron.py
+++ codes/neural/perceptron.py
@@ -5,37 +5,49 @@ import tensorflow as tf
 
 
 class Layer:
-    def __init__(self, input_shape, weights=None, biases=None):
+    def __init__(self, output_shape, input_shape, weights=None, biases=None):
+
+        self.input = None
+        self.output = None
+        self.output_shape = output_shape # N neurons
         self.input_shape = input_shape
         self.weights = self.init_weights() if weights is None else weights
         self.biases = self.init_biases() if biases is None else biases
 
     def init_weights(self):
-       return np.random.rand(self.input_shape[0], self.input_shape[1])
+       return np.random.rand(self.input_shape, self.output_shape)
     
     def init_biases(self):
-        return np.random.rand(self.input_shape[0], self.input_shape[1])
-       
+        return np.random.rand(1, self.output_shape) 
+    
     def __getattributes__(self):
         return self.__dict__
 
-l = Layer((10,2))
-
-print(l.weights)
-# print(l.__getattributes__())
-
-
 class model:
-    def __init__(self, input, n_shape, weights=None, biases=None,
-                  loss=Loss_function.mean_squared_error,
-                  optimizer=Optimisers.gradient):
-        
-        self.n_shape = n_shape 
-        self.input = input
-        self.i_shape = input.shape
-        self.loss = loss
-        self.optimizer = optimizer
-        # if weights == None:
+    def __init__(self, input_x, input_y, size=3, hidden_size=2, epochs=10, learning_rate=0.1):
+        self.input_x = np.array(input_x)
+        self.input_y = np.array(input_y)
+        self.epochs = epochs
+        self.learning_rate = learning_rate
+        self.size = size
+        self.hidden_size = hidden_size
+        self.layers = []
 
 
+    def forward(self, layer, input):
+        layer.input = input
+        layer.output = Activation_function.g_tanh(np.dot(input, layer.weights) + layer.biases)
+
+        return layer.output
     
+
+
+    # def backward(self, y_true):
+        
+
+
+    def fit(self):
+        samples = self.input_y.size
+        # for i in range(self.epochs):
+        #     for j in range(len(self.layers)):
+                
